8ccdb2d919ce7b89203469c370d4fadc609727ca
Slava 4REAL <slava@4real.io>
2018-07-06 16:42:50 -0400

    event history

--- src/getAbi.js
+++ src/getAbi.js
@@ -1,6 +1,7 @@
 export default function getAbi(contractEntry) {
   if (contractEntry.web3Contract) {
     return contractEntry.web3Contract.options.jsonInterface
+    return contractEntry.web3Contract.abi
   } else {
     return contractEntry.abi
   }
